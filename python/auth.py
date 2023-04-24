a671b6c83f8b87bbdeb11945b821553baae4d36a
Junio Cezar <jcezar@cyral.com>
2023-04-24 09:27:54 -0300

    add sample auth code

---
+++ python/auth.py
@@ -0,0 +1,7 @@
+def sso(username, token, user_account = None):
+    prep_user_account = user_account
+    if prep_user_account is not None:
+        prep_user_account = f":{user_account}"
+    sc._jsc.hadoopConfiguration().set('fs.s3a.access.key', f"{username}:{token}{prep_user_account}")
+    sc._jsc.hadoopConfiguration().set('fs.s3a.secret.key', 'none')
+    sc._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.BasicAWSCredentialsProvider")
